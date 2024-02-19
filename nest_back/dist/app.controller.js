29958cf139b383d378c81244c53daa1b0a24879c
Tenshii <127928819+Toitoine17@users.noreply.github.com>
2024-02-19 10:58:35 +0100

    Add files via upload

--- nest_back/dist/app.controller.js
+++ nest_back/dist/app.controller.js
@@ -11,9 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppController = void 0;
 const common_1 = require("@nestjs/common");
+const app_service_1 = require("./app.service");
 let AppController = class AppController {
+    constructor(appService) {
+        this.appService = appService;
+    }
     getMessage() {
-        return 'Hello from NestJS!';
+        return this.appService.getMessage();
     }
 };
 exports.AppController = AppController;
@@ -24,6 +28,7 @@ __decorate([
     __metadata("design:returntype", String)
 ], AppController.prototype, "getMessage", null);
 exports.AppController = AppController = __decorate([
-    (0, common_1.Controller)()
+    (0, common_1.Controller)(),
+    __metadata("design:paramtypes", [app_service_1.AppService])
 ], AppController);
 //# sourceMappingURL=app.controller.js.map
--- nest_back/src/app.controller.ts
+++ nest_back/src/app.controller.ts
@@ -1,10 +1,13 @@
 // app.controller.ts
 import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get('/api/message')
   getMessage(): string {
-    return 'Hello from NestJS!';
+    return this.appService.getMessage();
   }
 }
